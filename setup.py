15550aa73e3f0ae65b91e4d84e1286d94fe45926
teloryfrozy <raptor.frozy@gmail.com>
2024-05-03 21:56:10 +0300

    Add setup.py and update pyproject.toml

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="meta_ai_api",
+    author="teloryfrozy",
+    description="🦙 A Python module for interacting with the Meta Llama models trained via Replicate's API. 🔓",
+    keywords="reverse-engineering, meta, replicate, llm, llama2, llama3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    project_urls={
+        "GitHub": "https://github.com/teloryfrozy/llama-wrapper",
+    },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "Brotli",
+        "certifi",
+        "charset-normalizer",
+        "click",
+        "idna",
+        "joblib",
+        "nltk",
+        "regex",
+        "requests",
+        "tqdm",
+        "urllib3",
+    ],
+)
